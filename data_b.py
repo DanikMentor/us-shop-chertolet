b43dd248ae838ffb46d8401af3817457320b78ce
Seni4Man <arsenijs.kopacs@gmail.com>
2026-01-16 12:38:24 +0200

    Data_b

--- data_b.py
+++ data_b.py
@@ -1,9 +1,10 @@
+# data_b.py
 import sqlite3
+
 def cr_d():
     conn = sqlite3.connect("data_b.db")
     cursor = conn.cursor()
 
-    cursor.execute("DROP TABLE IF EXISTS users")
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS users(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -15,11 +16,9 @@ def cr_d():
         Password TEXT NOT NULL,
         Num_i_p TEXT NOT NULL,
         Bonus TEXT NOT NULL
-        
     )
     """)
 
-    cursor.execute("DROP TABLE IF EXISTS products")
     cursor.execute("""
     CREATE TABLE IF NOT EXISTS products(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -28,36 +27,11 @@ def cr_d():
         Category TEXT NOT NULL,
         Brand TEXT NOT NULL,
         Date TEXT NOT NULL
-       
-        
     )
     """)
 
-    users = [
-        ("","","","","","","",""),
-        ("","","","","","","",""),
-        ("","","","","","","","")
-    ]
-    products = [
-        ("","","","",""),
-        ("","","","",""),
-        ("","","","","")
-    ]
-
-
-    try:
-        cursor.executemany(
-            "INSERT INTO users (Name, Lastname, Mail, Phone_n, Login, Password, Num_i_p, Bonus) VALUES (?, ?, ?, ?, ?, ?, ?, ?)",
-            users
-        )
-        cursor.executemany(
-            "INSERT INTO products (Pr_name, Price, Category, Brand, Date) VALUES (?, ?, ?, ?, ?)",
-            products
-        )
-
-    except Exception as a:
-        print(a)
     conn.commit()
     conn.close()
 
-cr_d()
+if __name__ == "__main__":
+    cr_d()
