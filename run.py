367b918a2ddfb6510ca3fe09c484ab42889774f2
Danila <danilam980@gmail.com>
2026-01-28 00:59:48 +0200

    все гуд

--- run.py
+++ run.py
@@ -1,17 +1,6 @@
 import os
 from app import app
 
-# Печатаем для диагностики
-print("--- ДИАГНОСТИКА ПУТЕЙ ---")
-print(f"Текущая рабочая директория: {os.getcwd()}")
-print(f"Папка с шаблонами по мнению Flask: {app.template_folder}")
-print(f"Существует ли папка шаблонов? {os.path.exists(app.template_folder)}")
-
-if os.path.exists(app.template_folder):
-    print(f"Файлы в папке шаблонов: {os.listdir(app.template_folder)}")
-else:
-    print("ВНИМАНИЕ: Flask не видит папку templates!")
-print("--------------------------")
 
 if __name__ == "__main__":
     app.run(debug=True)
